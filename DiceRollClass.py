d10683e3651469892aee12489b12c73d8e7bb60f
ktompky <k.tompkins005@gmail.com>
2019-02-05 17:11:39 -0600

    Adding replay to Dice Roll

--- DiceRollClass.py
+++ DiceRollClass.py
@@ -6,12 +6,21 @@ class Dice:
     def __init__(self, die_sides):
         self.die_sides = die_sides
         self.roll()
-        self.value = value
 
     def roll(self):
-        self.value = random.randrange(self.die_sides) + 1
-        return self.value
+        value = random.randint(1, self.die_sides + 1)
+        return value
+
+
+player_choice = input("Would you like to roll the dice? 'y' for yes or 'n' for no.  ")
+
+while player_choice == 'y':
 
 
 
-
+    attack_dice = Dice(6)
+    attack_roll = attack_dice.roll()
+    print(attack_roll)
+    player_choice = input("Would you like to roll again? 'y' for yes or 'n' for no. ")
+if player_choice == 'n':
+    print("Have a good day then! ")
